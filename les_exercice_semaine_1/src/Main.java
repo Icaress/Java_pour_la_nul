74fb9deab6653fe2b509d6544df464e76d00cdcf
Malars_Icaru <malarsicaru9@gmail.com>
2025-10-01 09:04:51 +0200

    exercice 3 a 7 du cours

--- les_exercice_semaine_1/src/Main.java
+++ les_exercice_semaine_1/src/Main.java
@@ -1,4 +1,5 @@
 import java.lang.annotation.Target;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -68,5 +69,67 @@ class deux_Methode{
     public void nouvelle_methode(Train t1,String mot){
         System.out.println("nouvelle méthode appelée");
         System.out.println(t1.vitesse + mot);
+    }
+}
+
+class main_2{
+    public static void main(String [] args){
+        String nom = args[0];
+        int age = Integer.parseInt(args[1]);
+
+        System.out.println("le nom : " + nom);
+        System.out.println("l'age : " + age);
+
+    }
+}
+
+class ex_4{
+    public static int valeur = 1;
+    public int valeur_1 = 2;
+    public final String str_1 = "je";
+    public static void incrementervaleur(){
+        valeur++;
+    }
+    public static void main(String[] args){
+        System.out.println(valeur);
+        //System.out.println(valeur_1);
+        //cette ligne marche pas car c'est pas un static donc c'est une variable d'un objet et non de la classe
+        //donc pour la faire marché il faut crée un objet de cette classe
+        ex_4 affiche_none_static = new ex_4();
+        System.out.println(affiche_none_static.valeur_1);
+        ex_4.incrementervaleur();
+        System.out.println(ex_4.valeur);
+        System.out.println(affiche_none_static.str_1);
+        //pas modifiable car c'est un final donc une valeur non modifiable d'après le cours
+    }
+}
+
+class Conducteur{
+    public static void main(String[] args){
+        Train t = new Train();
+        t.stopper();
+        System.out.println("vitesse du train : " + t.vitesse + " en_marche : " + t.en_marche);
+        t.accelere();
+        System.out.println("vitesse du train : " + t.vitesse + " en_marche : " + t.en_marche);
+    }
+}
+
+class ex_6{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("quelle est votre nom ?");
+        String nom = sc.nextLine();
+        System.out.println("Bienvenue : " + nom);
+        int entier = sc.nextInt();
+        System.out.println("entier saisie: " + entier);
+
+        sc.close();
+        System.out.println("ressource libérer");
+    }
+}
+
+class ex_7{
+    public static void main(String [] args){
+        
     }
 }
