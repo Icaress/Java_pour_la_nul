0a8e2856e9848d54834a980e65e4e1669fee7746
Malars_Icaru <malarsicaru9@gmail.com>
2025-10-02 21:36:03 +0200

    commit 02/10/2025
    exo jusqu'au 11

--- les_exercice_semaine_1/src/Main.java
+++ les_exercice_semaine_1/src/Main.java
@@ -130,6 +130,75 @@ class ex_6{
 
 class ex_7{
     public static void main(String [] args){
-        
+        int[] TabInt = new int[5];
+        int[] TabInt2 = new int[5];
+        String[] tabString = new String[5];
+        //c'est une version plus simple que la suivante sa permet de faire le for en entier
+        for (int j : TabInt) {
+            System.out.println(j);
+        }
+        String[] tabString2 = new String[4];
+        for (int i = 0;i < tabString2.length;i++){
+            System.out.println(tabString2[i]);
+        }
+        char[] tabchar = new char[5];
+        int size = tabchar.length;
     }
 }
+class ex_8_ex_9{
+    public static void main(String[] args){
+        int[] tabint = {4,56,66,45,25};//ex_9
+        String[] tab = {"als","dsf","scsx","asd"};
+        for(int i : tabint){
+            System.out.println(i);
+        }
+        for(String j : tab){
+            System.out.println(j);
+        }
+
+    }
+}
+class ex_10 {
+    public static void main(String[] args) {
+        int[][] tabint = {{152, 251, 5554, 12}
+                , {155, 155, 2485, 115}};
+        int[][] tabint2 = new int[2][];
+        tabint2[0] = new int[3];
+        tabint2[1] = new int[5];
+        tabint2[0][0] = 20;
+        tabint2[1][4] = 20;
+        for (int i = 0; i < tabint2.length; i++) {
+            for (int j = 0; j < tabint2[i].length; j++) {
+                System.out.println(tabint2[i][j]);
+            }
+        }
+        // sa c'est vraiment complexe c'est un doucle for each c'est simple genre c'est juste que tabint2 c'est les valeur de i de la première dimension
+        // et ints c'est la valeur des y c'est juste les deux facons de l'écrire je te conseille le prmier pour pour pas tomber dans la trappe
+        for (int[] ints : tabint2) {
+            for (int anInt : ints) {
+                System.out.println(anInt);
+            }
+        }
+    }
+}
+
+class exo_11{
+    public static void main(String[] args){
+        int[] tab_int = {1,2,3,4,6,6,8,7,6,9};
+        //1ère école
+        int[] tab_int1 = tab_int;
+        //2ème école
+        int[] tab_int2 = new int[tab_int.length];
+        for(int i = 0; i < tab_int2.length; i++){
+            tab_int2[i] = tab_int[i];
+            System.out.print(tab_int2[i]);
+            System.out.print(tab_int[i]);
+        }
+        tab_int[0] = 9;
+        for(int i = 0; i < tab_int2.length; i++) {
+            System.out.print(tab_int2[i]);
+            System.out.print(tab_int[i]);
+        }
+    }
+}
+    
